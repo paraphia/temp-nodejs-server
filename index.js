f001ed93592984d16cff769f61422c9af47018e2
paraphia <paraphianetworks@gmail.com>
2022-01-18 09:18:22 +0500

    Temp commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+let filename = '';
+let success = true;
+const server = http.createServer((req,res)=>{
+	let queryObject = url.parse(req.url, true).query;
+	let fname = req.url === '/' ? 'index.html' : req.url; 
+	let fpath = path.join(__dirname,'public', fname);
+	const fext = path.extname(fpath);
+	const notFoundPath = path.join(__dirname, 'public', '404.html');
+	let content_type = 'text/html';
+	switch(fext){
+		case '.js':
+			content_type = 'text/javascript'
+			break;
+		case '.css':
+			content_type = 'text/css'
+			break
+	}
+	if(!fext){
+		fpath += '.html'
+	}
+	res.writeHead(200,{
+		'Content-type': content_type
+	})
+	if(fs.existsSync(fpath)){
+		fs.readFile(fpath, (err,data)=>{
+			if(err){
+				throw err;
+			}
+			res.end(data);
+		})
+	}else{
+		fs.readFile(notFoundPath, (err,data)=>{
+			if(err){
+				throw err;
+			}
+			res.end(data);
+		})
+	}
+})
+server.listen(3000)
